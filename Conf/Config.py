19909edc24e63f501f1fe1c15ac3851409616310
viniitsharma <sharma.vineet609@gmail.com>
2024-06-15 11:57:30 +0100

    [ODV-0001: Connect Spark]

---
+++ Conf/Config.py
@@ -0,0 +1,19 @@
+Config = {
+    "PYTHONPATH" : "C:/Users/44776/anaconda3/python.exe",
+    "LOGPATH" : "Log/log.txt",
+    "LOGGINGLEVEL" : "INFO",
+    "APPNAME" : "TokyoOlympics",
+    "CONFIG_HIVEWRH" : "spark.sql.warehouse.dir", 
+    "CONFIG_HIVEDIR" : "/user/hive/warehouse",
+    "SPARK_JARS" : "spark.jars",
+    "MYSQL_JARS" : "C:\spark-3.5.1-bin-hadoop3\jars",
+    "ENCODING_PATH" : "Encodings/CountryEncoding.json",
+    "INPUT_FILE_DIRECTORY" :"Data",
+    "HDFS_ETL_DIRECTORY" : "hdfs://localhost:9820/TokyoOlympics/t9",
+    "ENCODING_FILE_PATH" : "Encodings/CountryEncoding.json",
+    "ATHLETE_HDFS_FILE" : "Athletes.orc",
+    "COACH_HDFS_FILE" : "Coaches.orc",
+    "ENTRIES_GENDER_HDFS_FILE" : "EntriesGender.orc",
+    "MEDALS_HDFS_FILE" : "Medals.orc",
+    "TEAM_HDFS_FILE" : "Teams.orc"
+}
